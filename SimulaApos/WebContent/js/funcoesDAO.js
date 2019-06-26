eaf2b547a941a38210e1a0dd624eb6184544a0b6
Trajano <Trajano@Trajano-PC>
2019-06-26 16:28:47 -0300

    Feitos os comandos básicos

---
+++ SimulaApos/WebContent/js/funcoesDAO.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	function resultado(){
+		
+		var n1 = documento.queryselector(".n1").value;
+		var n2 = documento.queryselector(".n2").value;
+	
+		var resultado = parseInt(n1) + parseInt(n2);
+		
+		document.write(".resultado").innerHtml = resultado;
+		
+		
+		
+	};
+	
+	
+	
+	
+	
+	
+	
+})
+
+
