a7a33f3c8b78eef3df8b50b37f1ee5b32d43f913
Trajano <Trajano@Trajano-PC>
2019-06-26 17:44:58 -0300

    Feitos os cálculos

--- SimulaApos/WebContent/js/funcoesDAO.js
+++ SimulaApos/WebContent/js/funcoesDAO.js
@@ -1,24 +1,51 @@
-$(document).ready(function(){
 
-	function resultado(){
+	function clicar(){
 		
-		var n1 = documento.queryselector(".n1").value;
-		var n2 = documento.queryselector(".n2").value;
+		var num1 = document.querySelector(".n1").value;
+		var num2 = document.querySelector(".n2").value;
 	
-		var resultado = parseInt(n1) + parseInt(n2);
+		var resultado = parseInt(num1)+parseInt(num2);
+		var dif = 85 - resultado;
+	/*	document.querySelector(".resultado").innerHtml = resultado;*/
 		
-		document.write(".resultado").innerHtml = resultado;
+		if(num1 < 18){
+			document.write("Somente idades superiores a 18 anos");
+			return;
+		} else if(num1 > 100){
+			document.write("Somente idades com menos de 100 anos");
+			return;
+			
+		} else if(num2 < 15){
+			document.write("Tempo de contribuição mínimo de 15 anos");
+			
+		} else if(sexo="Fem"){
+			
+			if(resultado >= 85){
+				document.write("Parabéns, Vovó! Já pode se aposentar");
+				
+			} else {
+			
+				document.write("ainda faltam " + dif + " anos");
+				
+			}
+			
+		} else {
+			
+			if(resultado >= 95){
+				document.write("Parabéns, Vovô! Já pode se aposentar");
+				
+			} else {
+				var dif = 95 - resultado
+				document.write("ainda faltam " + dif + " anos");
+				
+			
+			
+		}
 		
-		
-		
+		}
+	
+	
 	};
 	
-	
-	
-	
-	
-	
-	
-})
 
 
