2ee794e7335c44c044b07cb025860c22493f6524
Trajano <Trajano@192.168.0.103>
2019-07-01 00:09:32 -0300

    feito tratamento de erro de imagem

--- SimulaApos/WebContent/js/funcoesDAO.js
+++ SimulaApos/WebContent/js/funcoesDAO.js
@@ -1,12 +1,43 @@
 
-	function clicar(){
+	
+
+
+
+
+	/*quando tem erro na imagem carregada/quebrada*/
+			
+	
+	$('img').on("error", function(){
+		
+		$('img').attr('src','img/error.jpg');
+	});
+	
+	function imgError(image) {
+	    image.onerror = "";
+	    image.src = "img/error.jpg";
+	    return true;
+	}
+	
+/*
+	$(function(){ clicar(){
 		
 		var num1 = document.querySelector(".n1").value;
 		var num2 = document.querySelector(".n2").value;
 	
 		var resultado = parseInt(num1)+parseInt(num2);
-		var dif = 85 - resultado;
-	/*	document.querySelector(".resultado").innerHtml = resultado;*/
+	
+		document.querySelector(".resultado").innerHtml = resultado;
+		
+		if(num1 <1 || num2< 0){
+			
+			document.write("Pelo menos um dos valores está vazio ou é menor do que zero");
+			return;
+		} 
+		
+		if (num2>num1  && num2-num1 > 0|| num1-num2<18){
+			document.write("Tempo de contribuição inválido");
+			return;
+		}
 		
 		if(num1 < 18){
 			document.write("Somente idades superiores a 18 anos");
@@ -17,15 +48,20 @@
 			
 		} else if(num2 < 15){
 			document.write("Tempo de contribuição mínimo de 15 anos");
+			return;
+			
 			
 		} else if(sexo="Fem"){
 			
 			if(resultado >= 85){
-				document.write("Parabéns, Vovó! Já pode se aposentar");
+				document.write("Parabéns, Vovó! Já pode se aposentar");	
+				 document.getElementById('vovoh');
+				
+				
 				
 			} else {
-			
-				document.write("ainda faltam " + dif + " anos");
+				var dif = 85 - resultado;
+				document.write("Que pena vovó... ainda faltam " + dif/2 + " anos");
 				
 			}
 			
@@ -33,10 +69,12 @@
 			
 			if(resultado >= 95){
 				document.write("Parabéns, Vovô! Já pode se aposentar");
+				var aposent = 2;
+				
 				
 			} else {
 				var dif = 95 - resultado
-				document.write("ainda faltam " + dif + " anos");
+				document.write("Que pena vovô... ainda faltam " + dif/2 + " anos");
 				
 			
 			
@@ -44,8 +82,7 @@
 		
 		}
 	
-	
+	return;
 	};
-	
-
-
+	})
+*/
